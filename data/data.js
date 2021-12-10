bec664ce9d99f1f12935ce56bb286aa012125820
Fran Mosteiro <fran.mosteiro@sequra.es>
2021-12-10 15:14:01 +0100

    Speakers info and images

--- data/data.js
+++ data/data.js
@@ -23,7 +23,7 @@ module.exports =
     },
     "home": {
         "title": "Nuestro programa",
-        "description": "Por noveno año consecutivo, profesionales referentes en el sector tecnológico a nivel nacional e internacional vendrán a Bilbao a compartir sus conocimientos y experiencia en los temas más punteros.",
+        "description": "Por décimo año 'consecutivo' (because COVID :/ ), profesionales referentes en el sector tecnológico a nivel nacional e internacional vendrán a Bilbao a compartir sus conocimientos y experiencia en los temas más punteros.",
         "agenda_cta_text": "Ver la agenda"
     },
     "speakers": [
@@ -42,6 +42,142 @@ module.exports =
             "talk": {
                 "title": "Cómo señor Lobear bien fuerte",
                 "description": "Hablará de cosas de Señor Lobo, joder"
+            }
+        },
+        {
+            "visible": true,
+            "slug": "naiara-abaroa",
+            "company": "Front-end Designer @ Wise",
+            "image": "/speakers/nabaroa.png",
+            "name": "Naiara Abaroa",
+            "bio": "",
+            "social": {
+                "twitter": "https://twitter.com/nabaroa",
+                "linkedin": "https://www.linkedin.com/in/naiaraabaroa/",
+                "github": "https://github.com/nabaroa"
+            },
+            "talk": {
+                "title": "10 años de CSS en un pomodoro",
+                "description": "Bilbostack celebra 10 entregas y este es el mejor momento para hacer retrospectiva y ver qué nos ha deparado el CSS sobre todo en esta última década. Ha sido un camino precioso, bullicioso, si lo comparamos con el páramo desértico que fueron los primeros 15 años. Daremos un paseo integral, nos daremos cuenta de cuánto hemos aprendido y veremos las últimas novedades que el estándar nos tiene preparadas. Esto no ha hecho más que empezar."
+            }
+        },
+        {
+            "visible": true,
+            "slug": "pablo-garaizar",
+            "company": "Lecturer @ Universidad de Deusto",
+            "image": "/speakers/pabloG.png",
+            "name": "Pablo Garaizar",
+            "bio": "",
+            "social": {
+                "twitter": "https://twitter.com/PGaraizar",
+                "linkedin": "https://www.linkedin.com/in/pablogaraizar/",
+                "github": "https://github.com/txipi"
+            },
+            "talk": {
+                "title": "¿Cómo se diseña una experiencia lúdica?",
+                "description": "Diseñar juegos (de mesa, de rol, videojuegos, etc.) u otras experiencias lúdicas como escape rooms o similares está todavía a medio camino entre un arte y una ciencia. En esta charla repasaremos los fundamentos básicos del diseño de juegos (mecánicas, historias, tecnologías y experiencias) y los diferentes tipos de diversión que puede buscar quien juegue a nuestros juegos (difícil, fácil, relacional y con propósito). Además, veremos cómo empezar el diseño de un juego de mesa y qué fases deberemos llevar a cabo para terminarlo, desde el prototipado, testeo, edición, (micromecenazgo), producción y distribución."
+            }
+        },
+        {
+            "visible": true,
+            "slug": "cata-oyaneder",
+            "company": "Software Engineer @ DAZN",
+            "image": "/speakers/cataO.png",
+            "name": "Cata Oyaneder",
+            "bio": "",
+            "social": {
+                "twitter": "https://twitter.com/cataflu",
+                "linkedin": "https://www.linkedin.com/in/cataflu/",
+                "github": "https://github.com/cat2608"
+            },
+            "talk": {
+                "title": "Seeking to the Action",
+                "description": "Nuestro equipo se encarga de crear funcionalidades que enriquecen la experiencia del usuario como por ejemplo Watch Party con la que puedes crear una sala virtual y asistir un partido en directo con amigos. También nos encargamos de mostrar en tiempo real las estadísticas de un partido de fútbol o de notificar al usuario cuando un partido está a punto de empezar. Pero todo esto empezó con la idea de marcar en la barra de tiempo los puntos claves de un partido de fútbol. En esta charla quiero compartir como hemos automatizado este proceso usando una serie de recursos de AWS como Step Functions, Lambdas, ECS, Rekognition y provisionando con Terraform."
+            }
+        },
+        {
+            "visible": true,
+            "slug": "edu-tomas",
+            "company": "Developer @ Plain Concepts",
+            "image": "/speakers/eduT.png",
+            "name": "Edu Tomás",
+            "bio": "",
+            "social": {
+                "twitter": "https://twitter.com/eiximenis",
+                "linkedin": "https://www.linkedin.com/in/etomas/",
+                "github": "https://github.com/eiximenis"
+            },
+            "talk": {
+                "title": "Kubernetes Do’s, don’ts and why’s",
+                "description": "Kubernetes está de moda, sin duda alguna. Casi todo el mundo se plantea si debería usarlo o no y qué ventajas o inconvenientes le aporta. En esta charla veremos un rápido vistazo a lo qué ofrece Kubernetes, pero también lo que NO hace, así como algunos temas a los que deberías prestar atención si te planteas usarlo."
+            }
+        },
+        {
+            "visible": true,
+            "slug": "bea-martin",
+            "company": "Chief Digital Officer (CDO) @ BBDO-España",
+            "image": "/speakers/beaM.png",
+            "name": "Bea Martín",
+            "bio": "",
+            "social": {
+                "twitter": "https://twitter.com/zigiella",
+                "linkedin": "https://www.linkedin.com/in/beatrizmartin/",
+                "github": "https://github.com/zigiella"
+            },
+            "talk": {
+                "title": "Qué Metaverso ni qué Metaversa",
+                "description": "¿Qué será el Metaverso? ¿Por qué será importante? ¿Por qué es importante ahora? Esta charla va de hacer una reflexión escepticoptimista sobre lo viejo y lo nuevo del concepto, lo que es y lo que no es. De analizar si la nueva era de Internet será entrar y vivir en él. De estimar cuándo va a suceder este cambio. Y cómo este cambio va a afectar a nuestros comportamientos de ocio, sociales, laborales y de consumo. Descomponer la convergencia tecnológica que lo hará posible, y la especial integración de los NFTs y las criptomonedas en esta evolución. No solo para saber observar la carrera que las Big Tech están emprendiendo ahora, sino para entender las oportunidades que existen para que nosotros seamos parte de este progreso."
+            }
+        },
+        {
+            "visible": true,
+            "slug": "carlos-hernandez",
+            "company": "CEO @ Quaderno",
+            "image": "/speakers/carlosH.png",
+            "name": "Carlos Hernández",
+            "bio": "",
+            "social": {
+                "twitter": "",
+                "linkedin": "",
+                "github": ""
+            },
+            "talk": {
+                "title": "",
+                "description": ""
+            }
+        },
+        {
+            "visible": true,
+            "slug": "teodora-bozheva",
+            "company": "",
+            "image": "/speakers/teodoraB.png",
+            "name": "Teodora Bozheva",
+            "bio": "",
+            "social": {
+                "twitter": "",
+                "linkedin": "",
+                "github": ""
+            },
+            "talk": {
+                "title": "",
+                "description": ""
+            }
+        },
+        {
+            "visible": true,
+            "slug": "jorge-uriarte",
+            "company": "",
+            "image": "/speakers/jorgeU.png",
+            "name": "Jorge Uriarte",
+            "bio": "",
+            "social": {
+                "twitter": "",
+                "linkedin": "",
+                "github": ""
+            },
+            "talk": {
+                "title": "",
+                "description": ""
             }
         }
     ],
@@ -171,11 +307,6 @@ module.exports =
                     "name": "Comalatech",
                     "link": "https://comalatech.com",
                     "image": "/sponsors/comalatech.png"
-                },
-                {
-                    "name": "On4u",
-                    "link": "https://www.on4u.es/",
-                    "image": "/sponsors/on4u.png"
                 },
                 {
                     "name": "Lookiero",
